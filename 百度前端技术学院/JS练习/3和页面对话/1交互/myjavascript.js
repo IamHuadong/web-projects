ac592e4d26502a0496574a0ed994d11383a75021
IamHuadong <chenhuadong94@outlook.com>
2019-08-10 21:09:54 +0800

    增加预览地址

--- 百度前端技术学院/JS练习/3和页面对话/1交互/myjavascript.js
+++ 百度前端技术学院/JS练习/3和页面对话/1交互/myjavascript.js
@@ -1,10 +1,10 @@
 document.getElementById("submit-btn").onclick = function(){
-	console.log(document.getElementById("name").value);
+	alert(document.getElementById("name").value);
 }
 document.getElementById("name").onkeyup = function(event){
 	// 按下enter
 	if(event.keyCode == 13){
-		console.log(document.getElementById("name").value);
+		alert(document.getElementById("name").value);
 	}
 	// 按下esc
 	else if(event.keyCode == 27){
